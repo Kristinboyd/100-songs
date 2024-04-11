98b1f5f5602ed398fa6adc28af69791e20b3eca3
Kristin Boyd <70916071+Kristinboyd@users.noreply.github.com>
2024-04-11 12:46:30 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,24 @@
+# Libraries
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+# Constants
+from secrets import *
+
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-modify-private",
+        redirect_uri="http://example.com",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt"
+    )
+)
+user_id = sp.current_user()["id"]
+date = input("Which year do you want to travel to? Type the date in this format YYYY-MM-DD: ")
+song_uris = ["The list of", "song URIs", "you got by", "searching Spotify"]
+
+playlist = sp.user_playlist_create(user=user_id, name=f"{date} Billboard 100", public=False)
+
+sp.playlist_add_items(playlist_id=playlist["id"], items=song_uris)
